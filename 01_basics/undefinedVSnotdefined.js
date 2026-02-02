c855920f0a9a12f813091afdbf05067321869da0
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-02 17:02:16 +0000

    What is undefined and what is not defined

---
+++ 01_basics/undefinedVSnotdefined.js
@@ -0,0 +1,7 @@
+console.log(x);
+// here output will be undefined because we are invoking x before its value was declared
+
+var x=7;
+
+console.log(a);
+// this will give NOT DEFINED because a was never created
