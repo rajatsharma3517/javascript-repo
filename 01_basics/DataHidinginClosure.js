11420115c69d08ba45194f872ab7e6687348d3ca
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-06 07:50:57 +0000

    see how variable count is encapsulised in fucntion counter so that no one can change this

---
+++ 01_basics/DataHidinginClosure.js
@@ -0,0 +1,11 @@
+function counter(){
+    var count = 0;
+    return function incrementcounter(){
+        count++;
+        console.log(count);
+    }
+}
+
+var counter1 = counter()
+counter1();
+counter1();
