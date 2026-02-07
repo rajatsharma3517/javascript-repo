3c0beccf265929e09664bee0a4c1c89367ce6729
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-07 17:05:04 +0000

    #

--- 01_basics/parametersVSarguments.js
+++ 01_basics/parametersVSarguments.js
@@ -1,6 +1,7 @@
+// here below while declaring function a, we passed parameters
 function a(x,y){
     var sum = x + y;
     return sum;
 }
-
+// while invoking or calling function we gave arguments 
 console.log(a(9,8));
