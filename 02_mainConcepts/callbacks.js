36cee149b18c3ffdb42f6f887999a33058268795
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-16 16:27:37 +0000

    Eventlisteners and callbacks

---
+++ 02_mainConcepts/callbacks.js
@@ -0,0 +1,6 @@
+// HTML CODE : <button id="clickMe">Click Me</button>
+
+document.getElementById("clickMe")
+    .addEventListener("click", function xyz() { 
+        console.log("Button Clicked"); 
+    });
