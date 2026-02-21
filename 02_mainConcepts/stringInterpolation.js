3bc618e9757c3f30430feb016c8d85410cd293bb
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-21 13:33:03 +0000

    String interpolation method , a new way to concatenate strings

---
+++ 02_mainConcepts/stringInterpolation.js
@@ -0,0 +1,8 @@
+let name = "Rajat Sharma";
+let salary = 300000
+
+console.log(`My name is ${name} and i earn ${salary}`);
+
+//old method is
+
+console.log("My name is " + name + "and i earn " + salary);
