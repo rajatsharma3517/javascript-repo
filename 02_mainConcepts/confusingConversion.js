b0105cc33cd483d454d7cc6888c74c19c1fd3191
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-21 13:20:45 +0000

    a great concept but confusing too

---
+++ 02_mainConcepts/confusingConversion.js
@@ -0,0 +1,5 @@
+console.log( 1 + 2 + "3");
+//first 1+2=3 and then it will concatenate 3 and 3 = 33
+
+console.log( "1" + 2 + 3)
+//but here because of string is there first then upcoming whether its number but everthing will be treated as string means 1 2 3= 123
