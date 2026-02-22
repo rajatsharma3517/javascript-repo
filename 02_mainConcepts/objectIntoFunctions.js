89757e9a93e8b4736cba1c24be6f4ade0d7d9f16
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-22 06:44:19 +0000

    how objects are passed into functions

---
+++ 02_mainConcepts/objectIntoFunctions.js
@@ -0,0 +1,10 @@
+const object = {
+    name: "hitesh",
+    price: 300
+};
+
+function printObject(myObject) {
+    console.log(`my name is ${myObject.name} and i earn ${myObject.price}`);
+}
+
+printObject(object);
