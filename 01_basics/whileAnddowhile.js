6a7728671bd5c35e39c3fa42b9b93d55cf3e22c6
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-24 14:52:54 +0000

    how while and do while works in js

---
+++ 01_basics/whileAnddowhile.js
@@ -0,0 +1,11 @@
+let score = 1;
+while (score <= 3) {
+    console.log(`Score is ${score}`);
+    score++;
+}
+
+let points = 11;
+do {
+    console.log(`Points are ${points}`);
+    points++;
+} while (points <= 10);
