a35af287320898b75abf8c38846a6b72ce407a0e
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-25 17:47:21 +0000

    Document Object Model

---
+++ 02_mainConcepts/documentObjectModel.js
@@ -0,0 +1,5 @@
+// document object model basically the whole html body the page all comes in document and we can manipulate that all by using DOM
+
+document.getElementById('ANY-ID');
+
+// BASIC syntax how DOM starts
