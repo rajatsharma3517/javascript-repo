5c3b41ca5d1553ae9b30399715205a4f90d3874d
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-02-25 17:49:41 +0000

    query selector

--- 02_mainConcepts/documentObjectModel.js
+++ 02_mainConcepts/documentObjectModel.js
@@ -1,5 +1,8 @@
 // document object model basically the whole html body the page all comes in document and we can manipulate that all by using DOM
 
 document.getElementById('ANY-ID');
-
 // BASIC syntax how DOM starts
+
+document.querySelector('.ANY-CLASS-NAME');
+//How we can select any query and we use a dot because of class
+
