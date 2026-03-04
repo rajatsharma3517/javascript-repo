c6f342cabd0015101c5d85a69fd882535a856610
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-03-04 17:55:23 +0000

    u

--- 02_mainConcepts/confusingConversion.js
+++ 02_mainConcepts/confusingConversion.js
@@ -2,4 +2,4 @@ console.log( 1 + 2 + "3");
 //first 1+2=3 and then it will concatenate 3 and 3 = 33
 
 console.log( "1" + 2 + 3)
-//but here because of string is there first then upcoming whether its number but everthing will be treated as string means 1 2 3= 123
+//but here cuz of string is there first then upcoming whether its number but everthing will be treated as string means 1 2 3= 123
