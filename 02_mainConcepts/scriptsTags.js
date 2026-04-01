07a05549bfe8e8d31a82ced42273b9185aeaa73c
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-04-01 17:48:30 +0000

    tags

---
+++ 02_mainConcepts/scriptsTags.js
@@ -0,0 +1,12 @@
+//inline
+//<script>
+        function sayHello() {
+            alert("Hello from inline JavaScript!");
+        }
+        console.log("This code runs directly inside the HTML file.");
+    </script>
+
+
+
+//external
+//<script src="script.js"></script>
